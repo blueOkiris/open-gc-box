81061ebd664165782c99374bb08471878b636934
Dylan Turner <dylan.turner@tutanota.com>
2023-09-13 18:17:58 -0500

    ArduBox: just enough code to send umodified gcn
    Signed-off-by: Dylan Turner <dylan.turner@tutanota.com>

--- code/ArduBox/ArduBox.ino
+++ code/ArduBox/ArduBox.ino
@@ -1,9 +1,15 @@
+#include <ArduGcn.hpp>
+
+const int g_gcPin = 7;
+ardugcn::GCControllerData g_contData = ardugcn::defaultController();
 
 void setup(void) {
-
+    Serial.begin(9600);
 }
 
 void loop(void) {
-
+    if (!sendToConsole(g_gcPin, g_contData)) {
+        Serial.println(F("Failed to send controller data!"));
+    }
 }
 
